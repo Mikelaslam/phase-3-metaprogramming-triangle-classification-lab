77f3a8ccc389bdf15418dfbb55c8f301d816ab3d
MIKEL DAVIS <kipkorir385@gmail.com>
2023-02-17 09:39:26 +0300

    Completed task

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,30 @@
 class Triangle
-  # write code here
+  attr_accessor :a, :b, :c
+
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+  end
+
+  def kind
+    if invalid?
+      raise TriangleError
+    elsif a == b && b == c
+      :equilateral
+    elsif a == b || b == c || a == c
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  private
+
+  def invalid?
+    [a, b, c].min <= 0 || (a + b <= c) || (a + c <= b) || (b + c <= a)
+  end
+
+  class TriangleError < StandardError
+  end
 end
